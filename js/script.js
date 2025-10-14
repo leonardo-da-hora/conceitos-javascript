bd0f4ea4cf27ed1cffd6b0a970d9198f1baa6ef7
leo <lghora7@gmail.com>
2025-10-14 20:41:11 -0300

    primeiro comit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-console.log("Olá")
 
 let nomeCompleto = "Leonardo"
 let idade = 26
@@ -8,12 +7,12 @@ let peso = null
 
 const altura = 1.68
 
-console.log(nomeCompleto, idade, temHabilitacao, altura, peso )
-
 let carroCor = "vermelho"
 let carroAno = 2018
 let carroModelo = "onix"
 let carroMotor = 1.6
+
+
 
 let carro = {
     cor: "vermelho",
@@ -22,4 +21,45 @@ let carro = {
     motor: 1.6 
 }
 
-console.log(carro)
+
+let cliente = {
+    nome: "Marcelo",
+    idade: 24,
+    altura: 1.93,
+    sabeProgramar: true
+}
+
+// Vetor
+
+let amigos = ["Maria", "João", "Winny", "Julia", "Santiago", "Castiel"];
+amigos[1] = "Marinho";
+
+
+// Funções
+
+function validaVenda(status, valor){
+    console.log("Sua venda foi: " + status + " o valor foi: ", + valor);
+}
+
+// validaVenda("Aprovada", 200);
+
+function desconto(valor){
+    let valorDesconto = valor - (valor * 0.20);
+    return valorDesconto;
+}
+
+let venda = 300;
+let vendaComDesconto = desconto(venda);
+console.log(vendaComDesconto);
+
+// Operador ternário
+
+let velocidade = 90;
+let limite = velocidade > 100 ? "Alta" : "Baixa";
+console.log(limite);
+
+// FOR 
+
+for(i = 9; i <= 10; i++){
+    console.log("Número: " + i);
+}
