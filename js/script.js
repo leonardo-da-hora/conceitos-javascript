ceb6407fcddc799f030bc4ccd67b510131125a96
leo <lghora7@gmail.com>
2025-10-13 21:06:15 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+console.log("Olá")
+
+let nomeCompleto = "Leonardo"
+let idade = 26
+let temHabilitacao = true
+
+let peso = null
+
+const altura = 1.68
+
+console.log(nomeCompleto, idade, temHabilitacao, altura, peso )
+
+let carroCor = "vermelho"
+let carroAno = 2018
+let carroModelo = "onix"
+let carroMotor = 1.6
+
+let carro = {
+    cor: "vermelho",
+    ano: 2018,
+    modelo: "onix",
+    motor: 1.6 
+}
+
+console.log(carro)
